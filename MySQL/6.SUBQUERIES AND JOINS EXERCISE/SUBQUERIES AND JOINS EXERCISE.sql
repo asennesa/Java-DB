5dc51358f193ac44afaccd91e308c27aa8b2bdcc
asennesa <angeorg117@gmail.com>
2020-02-06 16:03:26 +0200

    Done

--- MySQL/6.SUBQUERIES AND JOINS EXERCISE/SUBQUERIES AND JOINS EXERCISE.sql
+++ MySQL/6.SUBQUERIES AND JOINS EXERCISE/SUBQUERIES AND JOINS EXERCISE.sql
@@ -65,6 +65,28 @@ JOIN employees AS m ON e.manager_id = m.employee_id WHERE e.manager_id IN (3,7) 
  FROM mountains_countries AS mc  WHERE mc.country_code IN('US','BG','RU')
  GROUP BY mc.country_code
  ORDER BY mountain_range DESC;
+ 
+ # 14. Countries with Rivers
+ SELECT c.country_name,r.river_name
+ FROM countries AS C
+ LEFT  JOIN countries_rivers AS CR ON c.country_code = cr.country_code
+ LEFT  JOIN rivers AS r ON cr.river_id = r.id
+ WHERE c.continent_code ='AF'
+ ORDER BY c.country_name LIMIT 5;
+
+ # 16. Countries Without Any Mountains
+ Select COUNT(c.country_code) FROM countries AS c LEFT JOIN mountains_countries AS mc ON
+ mc.country_code = c.country_code WHERE mc.mountain_id IS NULL;
+ 
+ # 17. Highest Peak and Longest River by Country
+ SELECT c.country_name,MAX(p.elevation) AS highest_peak_elevation,
+ MAX(r.length) AS longest_river_length FROM countries AS c  
+ LEFT JOIN mountains_countries AS mc ON c.country_code = mc.country_code
+ JOIN peaks AS p ON p.mountain_id = mc.mountain_id
+ LEFT JOIN countries_rivers AS cr ON cr.country_code = c.country_code
+ JOIN rivers AS r ON r.id = cr.river_id
+ GROUP BY c.country_name
+ ORDER BY P.elevation DESC,r.length DESC,c.country_name ASC LIMIT 5;
 
 
 
