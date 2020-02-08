071be991fb91771122f39c00f47c4d5717a91fbb
asennesa <angeorg117@gmail.com>
2020-02-08 17:35:21 +0200

    Done

--- MySQL/8.EXAM PREP/EXAM PREP.sql
+++ MySQL/8.EXAM PREP/EXAM PREP.sql
@@ -67,7 +67,7 @@ FROM clients
 WHERE id BETWEEN 191 AND 200);
 
 # 3. Update
-UPDATE  employees_clients as ec 
+UPDATE  employees_clients AS ec 
 SET
 ec.employee_id = (SELECT ecc.employee_id  FROM 
 (SELECT *FROM employees_clients) AS ecc GROUP BY employee_id 
@@ -91,7 +91,7 @@ SELECT id ,CONCAT(first_name,' ',last_name) AS full_name,CONCAT('$',salary) AS s
 FROM employees WHERE salary >= 100000 AND started_on >=20180101 ORDER BY salary DESC,id;
 
 # 7. Cards against Humanity
-SELECT ca.id as id,CONCAT(ca.card_number,' : ',cl.full_name)as card_token 
+SELECT ca.id AS id,CONCAT(ca.card_number,' : ',cl.full_name)as card_token 
 FROM cards AS ca JOIN bank_accounts AS ba ON ca.bank_account_id = ba.id 
 JOIN clients AS cl ON cl.id = ba.client_id
 ORDER BY ca.id DESC;
@@ -101,9 +101,46 @@ SELECT concat(em.first_name,' ',em.last_name) AS name,em.started_on,COUNT(EC.cli
  FROM employees AS em 
 JOIN employees_clients AS ec ON em.id = ec.employee_id
 GROUP BY ec.employee_id
-ORDER BY count_of_clients desc, employee_id asc LIMIT 5;
+ORDER BY count_of_clients DESC, employee_id ASC LIMIT 5;
 
-# 9.
+# 9. Branch cards
+SELECT b.name,COUNT(C.ID) AS count_of_cards FROM branches AS b 
+ LEFT JOIN employees AS e ON b.id = e.branch_id 
+ LEFT JOIN employees_clients AS ec ON e.id = ec.employee_id
+ LEFT JOIN bank_accounts AS ba ON  ba.client_id = ec.client_id
+ LEFT JOIN cards AS c ON ba.id = c.bank_account_id
+ GROUP BY b.id ORDER BY count_of_cards DESC,b.name ASC;
+ 
+# 10. Extract client cards count
+ DELIMITER $$
+ CREATE FUNCTION udf_client_cards_count(name VARCHAR(30)) 
+RETURNS INT
+DETERMINISTIC
+BEGIN
+	DECLARE numOfCards INT;
+  SET numOfCards :=  (SELECT COUNT(ca.id) AS cards FROM clients AS c 
+JOIN bank_accounts AS ba ON c.id = ba.client_id
+JOIN carDs AS ca ON ba.id = ca.bank_account_id
+WHERE c.full_name LIKE name
+group by c.id);
+  
+	RETURN numOfCards;
+END $$
+
+# 11. Extract Client Info
+DELIMITER $$
+ CREATE PROCEDURE udp_clientinfo  (full_name VARCHAR(50))
+ BEGIN
+ SELECT c.full_name ,c.age,ba.account_number,Concat('$',ba.balance) AS balance
+ FROM clients AS c JOIN bank_accounts AS ba ON c.id = ba.client_id WHERE c.full_name LIKE full_name ;
+ 
+ END $$
+ 
+ CALL udp_clientinfo ('Hunter Wesgate');
+
+
+
+
 
 
 
