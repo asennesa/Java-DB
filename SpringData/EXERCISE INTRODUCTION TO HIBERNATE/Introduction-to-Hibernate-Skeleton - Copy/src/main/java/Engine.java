a6f3c74cabeb56a27282e132130f1071b6e7fb6f
asennesa <angeorg117@gmail.com>
2020-02-26 15:49:36 +0200

    Done

--- SpringData/EXERCISE INTRODUCTION TO HIBERNATE/Introduction-to-Hibernate-Skeleton - Copy/src/main/java/Engine.java
+++ SpringData/EXERCISE INTRODUCTION TO HIBERNATE/Introduction-to-Hibernate-Skeleton - Copy/src/main/java/Engine.java
@@ -1,5 +1,6 @@
 import entities.Address;
 import entities.Employee;
+import entities.Project;
 import entities.Town;
 
 import javax.persistence.EntityManager;
@@ -7,7 +8,11 @@ import javax.persistence.NoResultException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Engine implements Runnable {
 
@@ -58,6 +63,19 @@ public class Engine implements Runnable {
             e.printStackTrace();
         }
          */
+
+        //Ex 9
+        // findLatest10Projects();
+
+        //Ex 10
+        //increaseSalaries();
+
+        //Ex 11
+        try {
+            removeTowns();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
@@ -170,6 +188,92 @@ public class Engine implements Runnable {
                 .forEach(e -> System.out.println("\t" + e.getName()));
     }
 
-    
+    private void findLatest10Projects() {
+        List<Project> projects = this.entityManager
+                .createQuery("SELECT p FROM Project AS p ORDER BY p.startDate DESC", Project.class)
+                .setMaxResults(10).getResultList().stream()
+                .sorted((p1,p2)->p1.getName().compareTo(p2.getName())).collect(Collectors.toList());
+
+        projects.forEach(e -> {
+            try {
+                String endDate = e.getEndDate().toString();
+                System.out.println(String.format("Project name: %s" +
+                                "%n\tProject Description: %s" +
+                                "%n\tProject Start Date:%s" +
+                                "%n\tProject End Date: %s"
+                        , e.getName(), e.getDescription()
+                        , e.getStartDate().toString()
+                        , endDate));
+            }catch (NullPointerException npe){
+                String endDate = "null";
+                System.out.println(String.format("Project name: %s" +
+                                "%n\tProject Description: %s" +
+                                "%n\tProject Start Date:%s" +
+                                "%n\tProject End Date: %s"
+                        , e.getName(), e.getDescription()
+                        , e.getStartDate().toString()
+                        , endDate));
+            }
+
+
+
+        });
+
+    }
+
+    private void increaseSalaries(){
+        List<Employee> employees = this.entityManager.createQuery("SELECT e FROM Employee AS e " +
+                "WHERE e.department.name " +
+                "IN('Engineering','Tool Design','Marketing','Information Services')",Employee.class).getResultList();
+        this.entityManager.getTransaction().begin();
+        for (Employee employee : employees) {
+            BigDecimal twelvePercentIncrease = new BigDecimal("1.12");
+            this.entityManager.detach(employee);
+            employee.setSalary(employee.getSalary().multiply(twelvePercentIncrease));
+            this.entityManager.merge(employee);
+            System.out.println(String.format("%s %s ($%.2f)",employee.getFirstName()
+                    ,employee.getLastName(),employee.getSalary()));
+        }
+        this.entityManager.flush();
+        this.entityManager.getTransaction().commit();
+
+
+
+
+    }
+
+    private void removeTowns() throws IOException {
+        System.out.println("Enter Town Name: ");
+        String inputTown = reader.readLine();
+        Town town =  this.entityManager.createQuery("SELECT t FROM Town AS t WHERE t.name =:tname",Town.class)
+                .setParameter("tname",inputTown).getSingleResult();
+        int townId = town.getId();
+        List <Address> addresses = this.entityManager.createQuery("SELECT a FROM Address AS a " +
+                "WHERE a.town.id =:tId",Address.class).setParameter("tId",townId).getResultList();
+
+        List<Employee> employees = this.entityManager.createQuery("Select e FROM Employee AS e" +
+                " JOIN Address as a ON a.id=e.address.id " +
+                "JOIN Town as t ON t.id=a.town.id " +
+                "WHERE t.id =:tId",Employee.class).setParameter("tId",townId).getResultList();
+
+        this.entityManager.getTransaction().begin();
+        for (Employee e : employees) {
+            this.entityManager.detach(e);
+            e.setAddress(null);
+            this.entityManager.merge(e);
+        }
+        for (Address a : addresses) {
+            this.entityManager.remove(a);
+        }
+        this.entityManager.remove(town);
+        this.entityManager.flush();
+        this.entityManager.getTransaction().commit();
+
+        System.out.println(String.format("%d address in %s deleted",addresses.size(),town.getName()));
+
+    }
+
+
+
 
 }
