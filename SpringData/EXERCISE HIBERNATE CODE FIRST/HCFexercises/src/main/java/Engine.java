af606638fba366acfbd653f72ff99961c0c715c0
asennesa <angeorg117@gmail.com>
2020-03-02 13:47:21 +0200

    Done

---
+++ SpringData/EXERCISE HIBERNATE CODE FIRST/HCFexercises/src/main/java/Engine.java
@@ -0,0 +1,19 @@
+import relationsExercise.Course;
+import relationsExercise.Teacher;
+
+import javax.persistence.EntityManager;
+
+public class Engine implements Runnable {
+    private final EntityManager entityManager;
+
+    public Engine(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+
+}
---
+++ SpringData/EXERCISE HIBERNATE CODE FIRST/HCFexercises/src/main/java/entities/Sales/StoreLocation.java
@@ -0,0 +1,24 @@
+package entities.Sales;
+
+import entities.BaseEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "store_location")
+public class StoreLocation extends BaseEntity {
+    private String locationName;
+
+    public StoreLocation() {
+    }
+    @Column(name = "location_name")
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public void setLocationName(String locationName) {
+        this.locationName = locationName;
+    }
+}
